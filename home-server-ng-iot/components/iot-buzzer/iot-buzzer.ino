53163ae4e1dee1667eaa66a7b15e2061f113e7ee
Rodolphe <rodolphe.levexier@gmail.com>
2020-01-15 22:20:32 +0100

    premier pilotage crado pour le buzzer via le server

--- home-server-ng-iot/components/iot-buzzer/iot-buzzer.ino
+++ home-server-ng-iot/components/iot-buzzer/iot-buzzer.ino
@@ -13,18 +13,57 @@ const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
+// CONSTANTES
+// pin pour la led interne
 const int led = LED_BUILTIN;
+// pin pour le buzzer
+const int pinBuzzer = 5;
+const String moduleName = "homeserver-iot-buzzer";
+
+bool isBuzzerOn = false;
+
+void buzz() {
+  if(isBuzzerOn) {
+    for (int i = 0; i <80; i++)
+    {
+      digitalWrite (pinBuzzer, HIGH) ; //send tone
+      delay (1) ;
+      digitalWrite (pinBuzzer, LOW) ; //no tone
+      delay (1) ;
+    }
+    for (int i = 0; i <100; i++) 
+    {
+      digitalWrite (pinBuzzer, HIGH) ;
+      delay (2) ;
+      digitalWrite (pinBuzzer, LOW) ;
+      delay (2) ;
+    }
+  } else {
+    digitalWrite (pinBuzzer, HIGH) ;
+  }
+}
+
 
 void handleRoot() {
   digitalWrite(led, LOW);
-  server.send(200, "text/plain", "hello from esp8266!");
+  server.send(200, "text/plain", "hello from homeserver-iot-buzzer!");
   digitalWrite(led, HIGH);
 }
 
 void handleStatus() {
-  String message = "homeserver IOT module status\n";
+  String message = moduleName + " status\n";
   message += "IP address: " + WiFi.localIP().toString();
   server.send(200, "text/plain", message);
+}
+
+void handleBuzzerOn() {
+  isBuzzerOn = true;
+  server.send(200, "application/json", "{buzzer:true}");
+}
+
+void handleBuzzerOff() {
+  isBuzzerOn = false;
+  server.send(200, "application/json", "{buzzer:false}");
 }
 
 void handleNotFound() {
@@ -45,9 +84,13 @@ void handleNotFound() {
 }
 
 void setup(void) {
+  // configuration des pins
   pinMode(led, OUTPUT);
+  
+  pinMode(pinBuzzer, OUTPUT);
+
+  
   digitalWrite(led, HIGH);
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -55,49 +98,28 @@ void setup(void) {
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
   server.on("/status", handleStatus);
+  server.on("/buzzer/on", handleBuzzerOn);
+  server.on("/buzzer/off", handleBuzzerOff);
 
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
-
-  server.on("/gif", []() {
-    static const uint8_t gif[] PROGMEM = {
-      0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
-      0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
-      0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
-      0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
-      0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b
-    };
-    char gif_colored[sizeof(gif)];
-    memcpy_P(gif_colored, gif, sizeof(gif));
-    // Set the background to a random set of colors
-    gif_colored[16] = millis() % 256;
-    gif_colored[17] = millis() % 256;
-    gif_colored[18] = millis() % 256;
-    server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
-  });
-
+  
   server.onNotFound(handleNotFound);
 
   server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop(void) {
   server.handleClient();
   MDNS.update();
+  buzz();
 }
