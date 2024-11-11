4149e49f2cfc176d845ad997a61be0a611a85657
rodolphe_levexier <rodolphe_levexier@connect-tech.sncf>
2024-11-11 17:42:02 +0100

    bump musicplayer

--- home-music-player/src/packaging/install.sh
+++ home-music-player/src/packaging/install.sh
@@ -1,5 +1,5 @@
 sudo systemctl stop homemusicplayer.service
-rm /home/luna/SHARE/musicplayer/home-music-player-${project.version}.jar
+#rm /home/luna/SHARE/musicplayer/home-music-player-${project.version}.jar
 cp -r home-music-player-${project.version}.jar /home/luna/SHARE/musicplayer
 sudo cp homemusicplayer.service  /etc/systemd/system/homemusicplayer.service
 sudo systemctl daemon-reload
